900856526400e6b60687da4fb1d1580eb73df372
NikitaRusanovskii <nrusanovskii923@gmail.com>
2026-05-26 15:21:24 +0300

    feat(connector): /heartbeat

--- transport/connector.c
+++ transport/connector.c
@@ -157,9 +157,44 @@ CURLcode disconnect_from_signal_server() {
     return res;
 }
 
+CURLcode heartbeat() {
+    CURL *curl;
+    CURLcode res;
+    
+    struct MemoryResponse m;
+    init_memory(&m);
+
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    curl = curl_easy_init();
+
+    curl_easy_setopt(curl, CURLOPT_URL, ROUTE_URL("/heartbeat"));
+    curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "PATCH");
+    
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, NULL);
+    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, NULL);
+
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void*)&m);
+
+    res = curl_easy_perform(curl);
+    if (res == CURLE_OK) {
+        printf("Response: %s\n", m.memory);
+    }
+    else {
+        fprintf(stderr, "Error: %s\n", curl_easy_strerror(res));
+    }
+
+    free_memory(&m);
+    curl_easy_cleanup(curl);
+    curl_global_cleanup();
+
+    return res;
+}
+
+
 int main() {
     connect_to_signal_server("master");
-    disconnect_from_signal_server();
+    heartbeat();
 
     return 0;
 }
